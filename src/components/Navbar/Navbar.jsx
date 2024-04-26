807d0e76a1c1331b416e90ef3153f7a2f64c3d07
viktorherman55 <viktorherman55@gmail.com>
2024-04-26 16:52:32 +0200

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import imgages from '../../constants';
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
+  <div className='app_navbar'>
+    <div className='app_navbar-logo'>
+      <img src={imgages.gericht} alt="app logo" />
+    </div>
+    <ul className='app_navbar-links'>
+      <li className='p_opensans'><a href="#home">Home</a></li>
+      <li className='p_opensans'><a href="#about">About</a></li>
+      <li className='p_opensans'><a href="#menu">Menu</a></li>
+      <li className='p_opensans'><a href="#awards">Awards</a></li>
+      <li className='p_opensans'><a href="#contact">Contact</a></li>
+    </ul>
+    <div className='app_navbar-login'>
+      <a href="#login" className='p-opensans'>Login / Register</a>
+      <div/>
+      <a href="/" className='p_opensans'>Book Table</a>
+    </div>
   </div>
 );
 
