b48bc79802a4edd3076e3239ff1ea4d5d51924e8
viktorherman55 <viktorherman55@gmail.com>
2024-04-26 15:55:33 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Navbar } from './components';
+import '.App.css';
+import { AboutUs, Chef,FindUs,Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
   )
 }
 
