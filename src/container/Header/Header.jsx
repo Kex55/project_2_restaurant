9733a2ae7f42310cdb737be4b948d46a21bff455
viktorherman55 <viktorherman55@gmail.com>
2024-04-27 17:41:18 +0200

    Header content

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,10 +6,14 @@ import './Header.css';
 const Header = () => (
   <div className='app_header app_wrapper section_padding' id="home">
     <div className='app_wrapper_info'>
-      
+      <SubHeading title='Chase the flavour' />
+      <h1 className='app_header-h1'>The Key To Fine Dining </h1>
+      <button type='button' className='custom_button'>Explore Menu</button>
     </div>
 
-    <div className='app_wrapper_img'></div>
+    <div className='app_wrapper_img'>
+      <img src={images.welcome} alt="head_img" />
+    </div>
   </div>
 );
 
