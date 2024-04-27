d94a3fda49371d4161a81cd57a59e7c730761b55
viktorherman55 <viktorherman55@gmail.com>
2024-04-27 17:24:28 +0200

    Header base

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app_header app_wrapper section_padding' id="home">
+    <div className='app_wrapper_info'>
+      
+    </div>
+
+    <div className='app_wrapper_img'></div>
   </div>
 );
 
