68e6b315b1c846db0533b0637693285f45a41a9b
viktorherman55 <viktorherman55@gmail.com>
2024-04-28 18:06:19 +0200

    about us overlay

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { images } from "../../constants";
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app_aboutus flex_center section_padding' id='about'>
+    <div className='app_aboutus-overlay flex_center'>
+      <img src={images.G} alt="G_overlay" />
+    </div>
   </div>
 );
 
