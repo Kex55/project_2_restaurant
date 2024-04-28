8400af4502a53669724d96d35053b32e1c0ba3fa
viktorherman55 <viktorherman55@gmail.com>
2024-04-28 19:41:34 +0200

    Menu item componenet

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = (title, price, tags) => (
+  <div className='app_menuitem'>
+    <div className='app_menuitem-head'>
+
+      <div className='app_menuitem-name'>
+        <p className='p_cormorant' style={{ color: '#dcca87'}}>{title}</p>
+      </div>
+
+      <div className='app_menuitem-dash'/>
+
+      <div className='app_menuitem-price'>
+        <p className='p_cormorant'>{price}</p>
+      </div>
+      
+    </div>
+
+    <div className='app_menuitem-sub'>
+      <p className='p_opensans' style={{ color: '#AAAAAA'}}>{tags}</p>
+    </div>
+    
   </div>
 );
 
