7df1aa8451b5860dfa693d1279de99c76195d407
viktorherman55 <viktorherman55@gmail.com>
2024-04-29 17:06:20 +0200

    Intro video overlay refactor

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -6,21 +6,22 @@ import './Intro.css';
 const Intro = () => {
  const [playVideo, setPlayVideo] = React.useState(false);
  const vidRef = React.useRef();
- 
+ const handleVideo = () => {
+  setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+  if (playVideo) {
+    vidRef.current.pause();
+  } else {
+    vidRef.current.play();
+  }
+ }
+
  return (
   <div className='app_video'>
       <video src={meal} ref={vidRef} type="video/mp4" loop controls={false} muted></video>
       <div className='app_video-overlay flex_center'>
         <div 
         className='app_video-overlay_circle flex_center'
-        onClick={()=>{
-          setPlayVideo(!playVideo);
-          if (playVideo) {
-            vidRef.current.pause();
-          } else {
-            vidRef.current.play();
-          }
-          }}
+        onClick={handleVideo}
         >
           {playVideo ? (<BsPauseFill color='#fff' fontSize={30} />) : (<BsFillPlayFill color='#fff' fontSize={30} />)}  
         </div>
