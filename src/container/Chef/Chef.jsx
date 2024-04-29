febcb8df9877a157bb3482906cfe9b26e41bf0e9
viktorherman55 <viktorherman55@gmail.com>
2024-04-29 16:18:07 +0200

    Chef content

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app_bg app_wrapper section_padding'>
+    <div className='app_wrapper_img app_wrapper_img-reverse'>
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className='app_wrapper_info'>
+      <SubHeading title="Chef's word"/>
+      <h1 className='headtext_cormorant'>What we belive in</h1>
+      <div className='app_chef-content'>
+        <div className='app_chef-content_quote'>
+          <img src={images.quote} alt="quote_image" />
+          <p className='p_opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+        </div>
+        <p className='p_opensans'> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
+      </div>
+    </div>
   </div>
 );
 
