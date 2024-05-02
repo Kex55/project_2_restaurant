9cc76f5322bf881a64a6662941595dd1a994ddbd
viktorherman55 <viktorherman55@gmail.com>
2024-05-02 15:14:33 +0200

    FindUs content

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app_bg app_wrapper section_padding' id='contact'>
+    <div className='app_wrapper_info'>
+      <SubHeading title='Contact' />
+      <h1 className='headtext_cormorant' style={{marginBottom: '3rem'}}>Find Us</h1>
+      <div className='app_wrapper-content'>
+        <p className='p_opensans'></p>
+        <p className='p_opensans' style={{ color: '#dcca87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className='p_opensans'>Mon - Fri: 12:00 am - 0:00 pm</p>
+        <p className='p_opensans'>Sat - Sun: 13:00 am - 03:00 pm</p>
+      </div>
+    </div>
   </div>
 );
 
